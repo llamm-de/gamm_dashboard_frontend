327c934be427e3f6d290f978b276b65d4bb9c746
Lukas <lamm@ifam.rwth-aachen.de>
2022-03-10 10:35:28 +0100

    Added data fetching from api

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container> {{ data }}</v-container>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+
+  data() {
+    return {
+      data: null,
+    };
+  },
+  async mounted() {
+    const api_url = "http://127.0.0.1:5000/data";
+    const response = await fetch(api_url);
+    const response_data = await response.json();
+    this.data = response_data.Payload;
+  },
+};
+</script>
