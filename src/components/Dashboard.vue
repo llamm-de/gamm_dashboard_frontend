33528bba0f5683eb317b6053ca7cfedbaeddee0f
Lukas <lamm@ifam.rwth-aachen.de>
2022-04-04 14:46:27 +0200

    Removed chartjs

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,29 +1,11 @@
 <template>
-  <v-container v-if="data">
-    <v-row
-      ><v-col class="col-12"><DeadlinesCard :data="data" /></v-col
-    ></v-row>
-    <v-row>
-      <v-col class="col-3"><RegistrationsCard :data="data" /></v-col>
-      <v-col class="col-9"><RegistrationsPerDayCard :data="data" /></v-col>
-    </v-row>
-    <v-row>
-      <!-- <v-col class="col-12"><RegistrationsPerDayCard :data="data" /></v-col> -->
-    </v-row>
-  </v-container>
+  <v-container v-if="data"> </v-container>
 </template>
 
 <script>
-import RegistrationsCard from "./Registrations_card.vue";
-import DeadlinesCard from "./Deadlines_card.vue";
-import RegistrationsPerDayCard from "./RegistrationsPerDay_card.vue";
 export default {
   name: "Dashboard",
-  components: {
-    RegistrationsCard,
-    DeadlinesCard,
-    RegistrationsPerDayCard,
-  },
+  components: {},
 
   data() {
     return {
@@ -35,12 +17,6 @@ export default {
     const response = await fetch(process.env.VUE_APP_API_URL);
     const response_data = await response.json();
     this.data = response_data.Payload;
-
-    // Calculate progress of participant registrations
-    this.data.participant_progress =
-      (response_data.Payload.registrations_total /
-        response_data.Payload.participants_goal) *
-      100;
   },
 };
 </script>
