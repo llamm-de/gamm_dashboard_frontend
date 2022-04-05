3cfcf1e1ef6d4a239a79413e10ae4f9d09dfeb28
Lukas <lamm@ifam.rwth-aachen.de>
2022-04-05 11:28:14 +0200

    implemented side drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <v-app-bar clipped-left app dark>
+      <v-app-bar-nav-icon dark @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="font-weight-bold text-h4"
         >GAMM2022</v-toolbar-title
       >
@@ -22,13 +23,42 @@
         <span class="font-weight-light">TOOL</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-      <!-- <v-switch v-model="$vuetify.theme.dark" label="Dark mode"></v-switch> -->
     </v-app-bar>
+
+    <!--  Side Drawer -->
+    <v-navigation-drawer clipped class="grey lighten-3" v-model="drawer" app>
+      <v-list nav dense>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon class="large">mdi-view-dashboard</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="text-subtitle-1">
+            Dashboard
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon class="large">mdi-map</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="text-subtitle-1"> Map </v-list-item-title>
+        </v-list-item>
+      </v-list>
+      <template v-slot:append>
+        <div class="pa-2 text-center">
+          <v-btn icon><v-icon large>mdi-github</v-icon></v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
 export default {
   name: "Navbar",
+  data() {
+    return {
+      drawer: false,
+    };
+  },
 };
 </script>
